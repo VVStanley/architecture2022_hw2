50c697e9c2312c6d09fa3159140bf64b5977f92a
wstanley <stanley_23@mail.ru>
2022-07-03 12:12:02 +0300

    Добавил вектору сравнение

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,7 +1,7 @@
 """Тестирование модуля vector"""
 import pytest
 
-from src.exceptions import ObjectNotMovableError
+from src.exceptions import ObjectIsNotVectorTypeError
 from src.vector import Vector
 
 
@@ -12,17 +12,17 @@ class TestVector:
         """Проверяем что векторы можно сложить"""
         a = Vector(12, 5)
 
-        with pytest.raises(ObjectNotMovableError) as exc_info:
+        with pytest.raises(ObjectIsNotVectorTypeError) as exc_info:
             Vector.plus(a, None)  # type: ignore
 
-        assert exc_info.typename == "ObjectNotMovableError"
-        assert str(exc_info.value) == "Этот объект не двигается"
+        assert exc_info.typename == "ObjectIsNotVectorTypeError"
+        assert str(exc_info.value) == "Type must be Vector"
 
-        with pytest.raises(ObjectNotMovableError) as exc_info:
+        with pytest.raises(ObjectIsNotVectorTypeError) as exc_info:
             Vector.plus(None, a)  # type: ignore
 
-        assert exc_info.typename == "ObjectNotMovableError"
-        assert str(exc_info.value) == "Этот объект не двигается"
+        assert exc_info.typename == "ObjectIsNotVectorTypeError"
+        assert str(exc_info.value) == "Type must be Vector"
 
     def test_plus(self) -> None:
         """Тестируем метод сложения векторов"""
@@ -44,12 +44,28 @@ class TestVector:
         assert c.x == 5
         assert c.y == 8
 
+    def test_not_eq(self) -> None:
+        """Проверяем что векторы не равны"""
+        a = Vector(1, 1)
+        b = Vector(2, 2)
+
+        eq_vectors = a == b
+
+        assert eq_vectors is False
+
+    def test_eq(self) -> None:
+        """Проверяем сравнение векторов"""
+        a = Vector(123, 555)
+        b = Vector(123, 555)
+
+        assert a == b
+
     def test_add_no_vector_type(self) -> None:
         """Проверяем что вызывается исключение если сложение не с вектором"""
         a = Vector(12, 5)
 
-        with pytest.raises(ObjectNotMovableError) as exc_info:
+        with pytest.raises(ObjectIsNotVectorTypeError) as exc_info:
             a + None  # type: ignore
 
-        assert exc_info.typename == "ObjectNotMovableError"
-        assert str(exc_info.value) == "Этот объект не двигается"
+        assert exc_info.typename == "ObjectIsNotVectorTypeError"
+        assert str(exc_info.value) == "Type must be Vector"
